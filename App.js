4d45d7c5db0eb5119fbed50c1fd40b29eb7617a0
unknown <dungk2pi@gmail.com>
2022-12-19 17:40:13 +0700

    hello

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './src/screens/WelcomeScreen';
+import HomeScreen from './src/screens/HomeScreen';
 export default function App() {
-  return <WelcomeScreen />;
+  return <HomeScreen />;
 }
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import styled from 'styled-components/native'
+import React from 'react'
+import LottieView from 'lottie-react-native';
+
+const HomeScreen = () =>{
+    return (<View>
+
+    </View>)
+} 
+export default HomeScreen;
