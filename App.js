a90ab345d92145570a6e4c4c691800e23130b1dc
NGUYENDINHATK2 <dinhatnguyen81@gmail.com>
2022-12-19 17:10:07 +0700

    ok

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import styled from 'styled-components/native'
-
+import Lottie from 'lottie-react-native';
 const AppContainer = styled.View`
   flex: 1;
   background-color: red;
